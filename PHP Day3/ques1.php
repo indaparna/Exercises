0dc379068fadc1cb451645843c9f4e46401f33f5
indaparna <40848336+indaparna@users.noreply.github.com>
2019-02-20 16:08:42 +0530

    Add files via upload

---
+++ PHP Day3/ques1.php
@@ -0,0 +1,4 @@
+<?php
+$arr=[6,4,6,8,9];
+echo current($arr);
+?>
---
+++ PHP Day3/ques10.php
@@ -0,0 +1,7 @@
+<?php 
+echo stristr("HelLo world", "ll", true).'<br>'; 
+echo stristr("Hello world", "ll").'<br>';
+echo strstr("HelLo world", "ll").'<br>';  
+
+echo strstr("Hello world", "ll",true).'<br>'; 
+?> 
